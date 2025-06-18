e5f721c1a45699bc32404bc884119b2cf697b005
Andrew Monthero <andrewmonthero21@gmail.com>
2025-06-18 13:29:01 +0530

    CTA content color

--- src/components/sections/CTA.jsx
+++ src/components/sections/CTA.jsx
@@ -5,10 +5,10 @@ export default function CTA() {
     <section className="h-full bg-[#8845df] px-[1.25rem] py-[6.25rem]">
       <div className="mx-auto flex max-w-[1440px] items-center">
         <div className="flex max-w-[400px] flex-col items-start gap-[1.25rem] md:gap-[2rem]">
-          <h3 className="font-heading text-[1.5rem] leading-[1.875rem] tracking-[-0.5px] text-white uppercase md:text-[3.5rem] md:leading-[58px]">
+          <h3 className="font-heading text-[1.5rem] leading-[1.875rem] tracking-[-0.5px] text-[#fff] uppercase md:text-[3.5rem] md:leading-[58px]">
             Our promise to you.
           </h3>
-          <p className="text-[0.938rem] leading-[1.5] font-semibold tracking-[-0.5px] whitespace-pre-line text-white md:text-[1.125rem]">
+          <p className="text-[0.938rem] leading-[1.5] font-semibold tracking-[-0.5px] whitespace-pre-line text-[#fff] md:text-[1.125rem]">
             Your brand launch should feel less like a checkbox and more like a
             career mic-drop. Let's make it one to remember.
           </p>
