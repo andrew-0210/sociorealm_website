f87c935e94467ebf251cf68231ef76f342c5686c
Andrew Monthero <andrewmonthero21@gmail.com>
2025-06-18 13:02:07 +0530

    button updated

--- src/components/sections/WhyUs.jsx
+++ src/components/sections/WhyUs.jsx
@@ -17,7 +17,7 @@ const WhyUs = () => {
           <span className="font-handwritten text-[2rem] leading-[1.25] font-bold tracking-tight text-[#AF4FBE] md:text-[3.5rem]">
             Okay fine, 153% of the time.
           </span>
-          <button className="min-w-[250px] cursor-pointer rounded-full border-[2px] border-[#004e39] bg-[#4fbe9f] py-[1rem] text-[1rem] font-extrabold uppercase shadow-[0_6px_0px_rgba(0,78,57,1)] transition-shadow duration-200 ease-in hover:shadow-none md:min-w-[342px] md:text-[1.125rem]">
+          <button className="font-heading min-w-[250px] cursor-pointer rounded-full border-[2px] border-[#004e39] bg-[#4fbe9f] py-[1rem] text-[1rem] text-[#0a3f31] uppercase shadow-[0_6px_0px_rgba(0,78,57,1)] transition-shadow duration-200 ease-in hover:shadow-none md:min-w-[342px] md:text-[1.125rem]">
             Know How
           </button>
         </div>
