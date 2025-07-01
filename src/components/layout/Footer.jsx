94cc4977bcd23d4959a3d1506a74d3fa53982166
Andrew Monthero <andrewmonthero21@gmail.com>
2025-07-01 19:05:45 +0530

    footer updated

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <footer className="h-full bg-[#000000] px-[1.25rem] py-[6.25rem]">
       <div className="flex flex-col md:flex-row md:items-center md:justify-between">
-        <div className="relative h-[8vh] w-dvw">
+        <div className="relative h-[10vh] w-[100%]">
           <Image
             urlEndpoint="https://ik.imagekit.io/imagestore0210"
             src="/sociorealm_footer.svg"
             alt="Sociorealm_logo"
             priority
             fill
-            className="object-contain invert"
+            className="object-fill invert"
           />
         </div>
       </div>
